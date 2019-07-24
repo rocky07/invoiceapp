89f7b87a7b582166bd4a8da3c164a94cc6e5040a
Ramesan <rc9qrjm@supervalu.com>
2019-07-24 11:08:44 -0500

    updated url

--- app/model/Invoices.js
+++ app/model/Invoices.js
@@ -5,8 +5,8 @@ Ext.define('InvoiceApp.model.Invoices',{
 		
         fields: ['id', 'name'],
         proxy: {
-            type: 'ajax',
-            url : '/data.txt',
+            type: 'jsonp',
+            url : 'http://cyclehouse.in.162-222-225-80.plesk-web6.webhostbox.net/data.txt',
             reader: {
                 type: 'json',
                 rootProperty: 'users'
